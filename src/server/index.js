95b72a6d545096fbfb2c981599a8812c05c528ca
Remus Mate <remus.mate@mi9.com.au>
2016-07-28 15:11:59 +1000

    back to spaces :(

--- src/server/index.js
+++ src/server/index.js
@@ -1,1 +1,11 @@
-console.log('up');
+import express from 'express';
+
+const app = express();
+
+app.get('/', (res) => {
+  res.send('Hello');
+});
+
+app.listen(3000, () => {
+  console.log('up');
+});
