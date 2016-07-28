be4401165bb8eb22b6617cbdbedb724ad093d69a
madou <laheen@gmail.com>
2016-07-28 15:03:08 +1000

    Adds starter client

---
+++ src/client/src/components/Main.js
@@ -0,0 +1,11 @@
+import { Component } from 'react';
+
+export default class AppView extends Component {
+  render() {
+    return (
+      <div className="index">
+        wat up son
+      </div>
+    );
+  }
+}
---
+++ src/client/src/views/App.js
@@ -0,0 +1,11 @@
+import 'normalize.css/normalize.css';
+import { Component } from 'react';
+import Main from '../components/Main';
+
+class App extends Component {
+  render() {
+    return <Main />;
+  }
+}
+
+export default App;
